58b242194f45a63a876c051bbd943cfef4e54eef
Ricardo Silva <ricardolmsilva@outlook.com>
2023-02-01 22:45:52 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function showSuggestionDescriptions() {
+  const prefix = "marmameter-dot";
+  const search = "good";
+
+  const elements = document.querySelectorAll(".marmameter-dot");
+  elements.forEach((dot) => {
+    const attributeKey = "data-original-title";
+    const attributeValue = dot
+      .getAttribute("data-original-title")
+      .split(" ")[1];
+    dot.parentElement.append(attributeValue);
+  });
+}
+
+function addListOfKeyworks() {
+  const table = document.querySelector("#listTable_container");
+  const firstColumnCells = table.querySelectorAll("td.dt-left");
+  let keywords = [];
+  firstColumnCells.forEach((k) => keywords.push(k.textContent));
+  table.insertAdjacentHTML("afterend", keywords.join(", "));
+}
+
+addListOfKeyworks();
+showSuggestionDescriptions();
